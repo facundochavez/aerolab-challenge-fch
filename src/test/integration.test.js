c833b64547ef9077f71e62168b9b411001b4f8c3
Facundo Chavez <101282065+facukun@users.noreply.github.com>
2024-10-13 11:56:01 -0300

    improve/google fonts version

---
+++ src/test/integration.test.js
@@ -0,0 +1,6 @@
+// THIS TEST CHECKS THAT IT IS POSSIBLE TO ADD THE MAXIMUM AMOUNT OF POINTS AND REDEEM THE CHEAPEST PRODUCT
+
+import '@testing-libary/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import Layout from '@/app/layout';
+
