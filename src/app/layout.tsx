abae12dc83b4ffa74f9af7acf804db252322427d
Facundo Chavez <101282065+facukun@users.noreply.github.com>
2024-10-13 02:36:13 -0300

    improve/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ import { Montserrat } from 'next/font/google';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
+  weight: ['400', '500', '600', '700', '800', '900'],
 });
 
 export default function RootLayout({
